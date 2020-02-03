cf66f07fd7ec1c5c009b007828c171cb019da7d7
martbry <martin_b91@hotmail.com>
2020-02-03 10:12:02 +0100

    Initial commit

---
+++ Oblig1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Oblig1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
