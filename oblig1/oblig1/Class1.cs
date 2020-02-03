e4e7c9157eb241b2e5c405cbd7c3835686a57cf4
martbry <martin_b91@hotmail.com>
2020-02-03 15:34:08 +0100

    oblig 1 start

---
+++ oblig1/oblig1/Class1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace oblig1
+{
+    public class Person
+    {
+        private static int Antall = 0;
+
+
+        private int _id;
+        private string _firstName;
+        private string _lastName;
+        private int? _birthYear;
+        private int? _deathYear;
+
+        private Person _mother;
+        private Person _father;
+
+
+        public Person(string firstName = "____", string lastName = "____", int? birthYear = null, int? deathYear = null)
+        {
+            this._id = Person.Antall;
+            this._firstName = firstName;
+            this._lastName = lastName;
+            this._birthYear = birthYear;
+            this._deathYear = deathYear;
+            Antall++;
+        }
+
+        public string Show()
+        {
+            //Vis info om personen, samt barn og foreldre?
+
+
+
+            var birthyear = _birthYear == null ? "" : $"Born: {_birthYear.ToString()}";
+
+            var mother = _mother == default(Person) ? "" : $"Mor: {_mother.Name()} (ID: {_mother.Id().ToString()})";
+            var father = _father == default(Person) ? "" : $"Far: {_father.Name()} (ID: {_father.Id().ToString()})";
+
+
+            return $"Name: {_firstName} {_lastName} (ID: {_id})\n" +
+                   $"{birthyear}\n" +
+                   $"{mother}\n" +
+                   $"{father}";
+        }
+
+        public string Name()
+        {
+            return $"{_firstName} {_lastName}";
+        }
+
+        public int Id()
+        {
+            return this._id;
+        }
+
+        public void Mother(Person mother)
+        {
+            this._mother = mother;
+        }
+
+        public void Father(Person father)
+        {
+            this._father = father;
+        }
+
+
+    }
+}
---
+++ oblig1/oblig1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace oblig1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Person> personer = new List<Person>();
+
+
+            personer.Add(new Person(firstName: "Sverre Magnus", birthYear: 2005));
+            personer.Add(new Person(firstName: "Ingrid Alexandra", birthYear: 2004));
+            personer.Add(new Person(firstName: "Haakon Magnus", birthYear: 1973));
+            personer.Add(new Person(firstName: "Mette-Marit", birthYear: 1973));
+            personer.Add(new Person(firstName: "Marius", lastName: "Borg Høiby", birthYear: 1997));
+            personer.Add(new Person(firstName: "Harald", birthYear: 1937));
+            personer.Add(new Person(firstName: "Sonja", birthYear: 1937));
+            personer.Add(new Person(firstName: "Olav", birthYear: 1903));
+
+
+            personer[0].Father(personer[2]);
+            personer[0].Mother(personer[3]);
+            personer[1].Father(personer[2]);
+            personer[1].Mother(personer[3]);
+            personer[4].Mother(personer[3]);
+            personer[2].Father(personer[5]);
+            personer[2].Mother(personer[6]);
+            personer[5].Father(personer[7]);
+
+
+           
+
+            foreach (var person in personer)
+            {
+                Console.WriteLine($"{person.Id()} {person.Name()}");
+            }
+
+
+        }
+    }
+}
+
+
+
+//var sverreMagnus = new Person(firstName: "Sverre Magnus", birthYear: 2005);
+//var ingridAlexandra = new Person(firstName: "Ingrid Alexandra", birthYear: 2004);
+//var haakon = new Person(firstName: "Haakon Magnus", birthYear: 1973);
+//var metteMarit = new Person(firstName: "Mette-Marit", birthYear: 1973);
+//var marius = new Person(firstName: "Marius", lastName: "Borg Høiby", birthYear: 1997);
+//var harald = new Person(firstName: "Harald", birthYear: 1937);
+//var sonja = new Person(firstName: "Sonja", birthYear: 1937);
+//var olav = new Person(firstName: "Olav", birthYear: 1903);
+
+
+//sverreMagnus.Father(haakon);
+//sverreMagnus.Mother(metteMarit);
+//ingridAlexandra.Father(haakon);
+//ingridAlexandra.Mother(metteMarit);
+//marius.Mother(metteMarit);
+//haakon.Father(harald);
+//haakon.Mother(sonja);
+//harald.Father(olav);
